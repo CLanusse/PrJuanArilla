fede7c5a7074d895e66f9d1d16acd55a45144f41
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-06-16 10:43:29 -0300

    Ejercicios para hacer

---
+++ ParaHacer/ejercicio1.js
@@ -0,0 +1,38 @@
+
+
+// ============= rango ==========
+// escribir una función RANGO que recibiendo dos números como parámetro, retorne
+// un arreglo con todos los números entre esos dos parámetros
+
+function rango(a, b) {
+    // hacer
+    // retornar un arreglo con todos los valores entre a y b
+}
+
+//ejemplo :
+//   rango(1, 5) == retorna =>    [1, 2, 3, 4, 5]
+
+
+
+
+
+// ============= suma ==============
+
+// esribir una funcion que tome un ARREGLO por parámetro y retorne 
+// la suma (número entero) de todos los valores de ese arreglo
+
+function suma( arreglo ) {
+    //recibe un arreglo por parámetro
+    // retorna la suma de todos los elementos del arreglo
+}
+
+// ejemplo:
+//   suma( [1, 2, 3, 4] ) == retorna ==>   10
+
+
+
+// combinar ambas funciones
+// ejemplo:
+// suma( rango(1, 5) ) == retorna => 15
+
+
---
+++ ParaHacer/ejercicio2.js
@@ -0,0 +1,12 @@
+// ===== revertir un arreglo ======
+// escribir una función revertirArreglo que reciba un array por parámetro
+// y retorne un arreglo distinto con todos los elementos en orden inverso
+
+function revertirArreglo( arreglo ) {
+    // debe retornar un arreglo con sus elementos en orden inverso
+    // pensar en métodos .push o .unshift para agregar elementos a otro arreglo
+}
+
+
+// ejemplo:
+// revertirArreglo( [1, 2, 3, 4] ) === retorna ==> [4, 3, 2, 1]
